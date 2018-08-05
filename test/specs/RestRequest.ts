b4b58c9288703176faa46b8700286407bcbe21d5
Thanh Truong <vagabondtt1503@gmail.com>
2018-08-05 15:22:25 -0400

    refactor code with data type

--- test/specs/RestRequest.ts
+++ test/specs/RestRequest.ts
@@ -1,16 +1,22 @@
-import { APIGatewayProxyResult, APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { APIGatewayProxyResult, APIGatewayProxyEvent, Context, APIGatewayProxyHandler } from 'aws-lambda';
 
 export interface RequestOptions {
   body?: any;
   token?: string;
 }
 
+export class ActionRequestOutput<T> {
+  constructor(public response: APIGatewayProxyResult, public body: T) {
+  }
+}
+
 export class RestRequest {
-  protected async CallRestAPI(RestFunction: Function, options: RequestOptions): Promise<APIGatewayProxyResult> {
-      const event: APIGatewayProxyEvent = {} as APIGatewayProxyEvent;
-      let context: Context;
-      event.body = JSON.stringify(options.body);
-      let returnValue = await RestFunction(event, context);
-      return returnValue;
+  protected async CallRestAPI<T>(RestFunction: APIGatewayProxyHandler, options: RequestOptions): Promise<ActionRequestOutput<T>> {
+    const event: APIGatewayProxyEvent = {} as APIGatewayProxyEvent;
+    let context: Context;
+    event.body = JSON.stringify(options.body);
+    let response = await RestFunction(event, context, undefined) as APIGatewayProxyResult;
+    const body = JSON.parse(response.body);
+    return { response, body };
   }
 }
--- test/specs/auth/auth-request.ts
+++ test/specs/auth/auth-request.ts
@@ -1,14 +1,15 @@
-import { RestRequest } from '../RestRequest';
+import { RestRequest, ActionRequestOutput } from '../RestRequest';
 import { login, register } from '../../../src/auth/auth';
-import { APIGatewayProxyResult } from 'aws-lambda';
+import { UserDataModel } from '../../../src/users/models/user-model';
+import { LoginOutputModel } from '../../../src/auth/models/auth-model';
 
 export  class AuthRestRequest extends RestRequest {
 
-  async callRegisterAPI(body: Object): Promise<APIGatewayProxyResult> {
-    return await this.CallRestAPI(register, { body });
+  async callRegisterAPI(body: Object): Promise<ActionRequestOutput<UserDataModel>> {
+    return await this.CallRestAPI<UserDataModel>(register, { body });
   }
 
-  async callLoginAPI(body: Object): Promise<APIGatewayProxyResult> {
-    return await this.CallRestAPI(login, { body });
+  async callLoginAPI(body: Object): Promise<ActionRequestOutput<LoginOutputModel>> {
+    return await this.CallRestAPI<LoginOutputModel>(login, { body });
   }
 }
