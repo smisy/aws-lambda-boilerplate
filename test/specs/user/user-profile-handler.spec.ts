2332eddbc6b02d8b0fdc9ce513b458415dad8a7d
Thanh Truong <vagabondtt1503@gmail.com>
2018-08-08 09:22:59 -0400

    make user profile test cases

---
+++ test/specs/user/user-profile-handler.spec.ts
@@ -0,0 +1,66 @@
+import { AuthRestRequest } from '../auth/auth-request';
+import { startMockgoose, resetMongoose } from '../../lib/mongoose';
+import { AuthResponseContext } from 'aws-lambda';
+import { UserDataModel } from '../../../src/users/models/user-model';
+import { RegisterInputModel } from '../../../src/auth/models/auth-model';
+import { ActionRequestOutput } from '../RestRequest';
+import { UserProfileRestRequest } from './user-profile-request';
+
+const newUser: RegisterInputModel = {
+  name: 'Test',
+  email: 'login@test.com',
+  password: 'Test@test123',
+  phone: '+14049756123'
+};
+
+let authRequest: AuthRestRequest;
+let registerResponse: ActionRequestOutput<UserDataModel>;
+let user: UserDataModel;
+let userProfileRequest: UserProfileRestRequest;
+describe('Get User Profile', () => {
+  before(async () => {
+    authRequest = new AuthRestRequest();
+    userProfileRequest = new UserProfileRestRequest();
+    await startMockgoose();
+    registerResponse = await authRequest.callRegisterAPI(newUser);
+    user = registerResponse.body;
+  });
+
+  afterEach(async () => {
+    await resetMongoose();
+  });
+
+  it('should return 200 get profile request with right authorizer', async () => {
+    let authorizer: AuthResponseContext = {
+      principalId: user.id
+    };
+    let { response, body } = await userProfileRequest.callGetProfileAPI(
+      authorizer
+    );
+    response.statusCode.should.be.equal(200);
+    body.should.have.property('id');
+  });
+
+  it('should return 422 get profile request with invalid user Id', async () => {
+    let authorizer: AuthResponseContext = {
+      principalId: '12345678'
+    };
+    let { response, body } = await userProfileRequest.callGetProfileAPI(
+      authorizer
+    );
+    response.statusCode.should.be.equal(422);
+    body.should.have.property('error');
+  });
+
+  it('should return 422 get profile request with empty user Id', async () => {
+    let authorizer: AuthResponseContext = {
+      principalId: undefined
+    };
+    let { response, body } = await userProfileRequest.callGetProfileAPI(
+      authorizer
+    );
+    response.statusCode.should.be.equal(422);
+    body.should.have.property('error');
+  });
+
+});
--- test/specs/user/user-profile-request.ts
+++ test/specs/user/user-profile-request.ts
@@ -1,10 +1,11 @@
 import { RestRequest, ActionRequestOutput } from '../RestRequest';
 import { UserProfileDataModel } from '../../../src/users/models/user-model';
 import { getProfile } from '../../../src/users/user';
+import { AuthResponseContext } from 'aws-lambda';
 
 export  class UserProfileRestRequest extends RestRequest {
 
-  async callHetProfileAPI(parameter: any): Promise<ActionRequestOutput<UserProfileDataModel>> {
-    return await this.CallRestAPI<UserProfileDataModel>(getProfile, { parameter });
+  async callGetProfileAPI(authorizer: AuthResponseContext): Promise<ActionRequestOutput<UserProfileDataModel>> {
+    return await this.CallRestAPI<UserProfileDataModel>(getProfile, { authorizer });
   }
 }
